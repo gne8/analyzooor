444b365e6e3d965321a381d919935a335078bdde
gne8 <gnehzspace@gmail.com>
2023-10-14 19:19:31 +0800

    Add data active days extractor

--- data_massagor.py
+++ data_massagor.py
@@ -29,7 +29,24 @@ def basic_stats_extractor(df):
         'total_unique_month_years': [total_unique_month_years]
     })
 
+def date_active_stats_extractor(df):
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df['date'] = df['datetime'].dt.date
+    unique_hash_counts = df.groupby('date')['hash'].nunique()
+
+    #Convert the Series to a DataFrame
+    df_unique_hash_counts = unique_hash_counts.reset_index()
+
+    #Rename the columns
+    df_unique_hash_counts.columns = ['date', 'count']
+
+    #Save the DataFrame as a CSV file
+    return df_unique_hash_counts
+
 for address in address_list:
     dir_name = f'data/{address}'
     df = basic_stats_extractor(pd.read_csv(f'{dir_name}/mnt_transactions_data.csv'))
     df.to_csv(f'{dir_name}/mnt_basic_stats.csv', index=False)
+
+    df = date_active_stats_extractor(pd.read_csv(f'{dir_name}/mnt_transactions_data.csv')) 
+    df.to_csv(f'{dir_name}/mnt_date_count_txn.csv', index=False)

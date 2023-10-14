a8fd1c1c6f84538d9d1bc98a58d43767e0a6bcc1
gne8 <gnehzspace@gmail.com>
2023-10-14 18:41:35 +0800

    Add data_massagor, add basic stats data

---
+++ data_massagor.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+address_list = [
+    os.getenv("ADDRESS_1"),
+    os.getenv("ADDRESS_2"),
+    os.getenv("ADDRESS_3"),
+    os.getenv("ADDRESS_4")
+]
+
+def basic_stats_extractor(df):
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    max_datetime = df['datetime'].max()
+    min_datetime = df['datetime'].min()
+    df['date'] = df['datetime'].dt.date
+    total_unique_days = df['date'].nunique()
+    total_transactions_count = df['hash'].nunique()
+    df['month_year'] = df['datetime'].dt.to_period('M')
+    total_unique_month_years = df['month_year'].nunique()
+
+    return pd.DataFrame({
+        'max_datetime': [max_datetime],
+        'min_datetime': [min_datetime],
+        'total_transactions_count': [total_transactions_count],
+        'total_unique_days': [total_unique_days],
+        'total_unique_month_years': [total_unique_month_years]
+    })
+
+for address in address_list:
+    dir_name = f'data/{address}'
+    df = basic_stats_extractor(pd.read_csv(f'{dir_name}/mnt_transactions_data.csv'))
+    df.to_csv(f'{dir_name}/mnt_basic_stats.csv', index=False)
